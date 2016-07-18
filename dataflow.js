f9a3864125dc2118e658baa8050d7faeab6991f2
arkenidar <dario.cangialosi@gmail.com>
2016-07-18 12:17:27 +0200

    minor changes

--- dataflow.js
+++ dataflow.js
@@ -1,44 +1,44 @@
-// reactive dataflow nand operation queue
-
+// dataflow_js (reactive dataflow nand operation queue)
+// a way of processing data which uses simple primitives
 function nand_operation(a, b){
   return !(a&&b);
 }
 
-memory=[false, false, false]
-operations=[[1], [0,2], []]
-queue=[0]
+memory=[false, false, false];
+operations=[[1], [0,2], []];
+queue=[0];
 
-log('queue')
+log('queue');
 while(true){
   // get source from queue
-  source=queue.shift()
+  source=queue.shift();
   if(typeof source=='undefined') break; // exits on empty queue
 
   // get destination
-  destinations=operations[source]
-  log('destinations')
+  destinations=operations[source];
+  log('destinations');
   
-  destinations.forEach(destination=>{ // for each destination
-    log('destination', destination)
+  destinations.forEach(destination => { // for each destination
+    log('destination', destination);
 
-    log('memory')
-    destination_before=memory[destination]
+    log('memory');
+    destination_before=memory[destination];
     // operation from source to destination
-    memory[destination]=nand_operation(memory[destination], memory[source])
-    destination_after=memory[destination]
+    memory[destination]=nand_operation(memory[destination], memory[source]);
+    destination_after=memory[destination];
 
     if(destination_after!=destination_before) // mutation event
       // add destination to queue (as new source) on mutation event
-      queue.push(destination)
+      queue.push(destination);
   })
-  log('queue')
+  log('queue');
 }
-log('memory')
+log('memory');
 
 function log(var_name, var_value=''){
   if(['queue'].indexOf(var_name)!=-1){
     console.log(var_name+': '+
       (var_value===''?global[var_name]:var_value)
-    )
+    );
   }
 }
