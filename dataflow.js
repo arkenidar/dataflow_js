30b9574a8e24433a04fbecf3a13f1085e75b4c83
arkenidar <dario.cangialosi@gmail.com>
2016-07-24 20:10:31 +0200

    spacing

--- dataflow.js
+++ dataflow.js
@@ -7,38 +7,38 @@ function nand_operation(a, b){
 
 function circuit(in_a, in_b){
 
-memory=[false, false, false];
-memory[0]=in_a; memory[1]=in_b;
-operations=[[1], [2], []];
-queue=[0];
+	memory=[false, false, false];
+	memory[0]=in_a; memory[1]=in_b;
+	operations=[[1], [2], []];
+	queue=[0];
 
-view('queue');
-while(true){
-	// get source from queue
-	source=queue.shift();
-	if(typeof source=='undefined') break; // exits on empty queue
-
-	// get destination
-	destinations=operations[source];
-	view('destinations');
-	
-	destinations.forEach(function(destination) { // for each destination
-		view('destination', destination);
-
-		view('memory');
-		destination_before=memory[destination];
-		// operation from source to destination
-		memory[destination]=nand_operation(memory[destination], memory[source]);
-		destination_after=memory[destination];
-
-		if(destination_after!=destination_before) // mutation event
-			// add destination to queue (as new source) on mutation event
-			queue.push(destination);
-	});
 	view('queue');
-}
-view('memory');
-return memory[2];
+	while(true){
+		// get source from queue
+		source=queue.shift();
+		if(typeof source=='undefined') break; // exits on empty queue
+
+		// get destination
+		destinations=operations[source];
+		view('destinations');
+		
+		destinations.forEach(function(destination) { // for each destination
+			view('destination', destination);
+
+			view('memory');
+			destination_before=memory[destination];
+			// operation from source to destination
+			memory[destination]=nand_operation(memory[destination], memory[source]);
+			destination_after=memory[destination];
+
+			if(destination_after!=destination_before) // mutation event
+				// add destination to queue (as new source) on mutation event
+				queue.push(destination);
+		});
+		view('queue');
+	}
+	view('memory');
+	return memory[2];
 
 }
 console.log(circuit(false, false));
