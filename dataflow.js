802af04037e08b75009cafe1c199c73bab98ff13
arkenidar <dario.cangialosi@gmail.com>
2016-07-24 20:08:50 +0200

    spacing

--- dataflow.js
+++ dataflow.js
@@ -1,8 +1,8 @@
 // dataflow_js (reactive dataflow nand operation queue)
 // a way of processing data which uses simple primitives
 function nand_operation(a, b){
-  var c = !(a&&b);
-  return c;
+	var c = !(a&&b);
+	return c;
 }
 
 function circuit(in_a, in_b){
@@ -14,28 +14,28 @@ queue=[0];
 
 view('queue');
 while(true){
-  // get source from queue
-  source=queue.shift();
-  if(typeof source=='undefined') break; // exits on empty queue
+	// get source from queue
+	source=queue.shift();
+	if(typeof source=='undefined') break; // exits on empty queue
 
-  // get destination
-  destinations=operations[source];
-  view('destinations');
-  
-  destinations.forEach(function(destination) { // for each destination
-    view('destination', destination);
+	// get destination
+	destinations=operations[source];
+	view('destinations');
+	
+	destinations.forEach(function(destination) { // for each destination
+		view('destination', destination);
 
-    view('memory');
-    destination_before=memory[destination];
-    // operation from source to destination
-    memory[destination]=nand_operation(memory[destination], memory[source]);
-    destination_after=memory[destination];
+		view('memory');
+		destination_before=memory[destination];
+		// operation from source to destination
+		memory[destination]=nand_operation(memory[destination], memory[source]);
+		destination_after=memory[destination];
 
-    if(destination_after!=destination_before) // mutation event
-      // add destination to queue (as new source) on mutation event
-      queue.push(destination);
-  });
-  view('queue');
+		if(destination_after!=destination_before) // mutation event
+			// add destination to queue (as new source) on mutation event
+			queue.push(destination);
+	});
+	view('queue');
 }
 view('memory');
 return memory[2];
@@ -47,9 +47,9 @@ console.log(circuit(true, false));
 console.log(circuit(true, true));
 
 function view(var_name, var_value=''){
-  if(['queue'].indexOf(var_name)!=-1){
-    console.log(var_name+': '+
-      (var_value===''?global[var_name]:var_value)
-    );
-  }
+	if(['queue'].indexOf(var_name)!=-1){
+		console.log(var_name+': '+
+			(var_value===''?global[var_name]:var_value)
+		);
+	}
 }
