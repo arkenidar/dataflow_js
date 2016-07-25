fc2c9f12252324869d7d284a7705d1eadf9d0830
Dario Cangialosi <Dario Cangialosi>
2016-07-26 00:04:28 +0200

    fixes from webstorm ide hints

--- dataflow.js
+++ dataflow.js
@@ -1,17 +1,16 @@
 var p = console.log;
 
-// dataflow_js (reactive dataflow nand operation queue)
+// data flow _js (reactive data flow negated-and universal_operation queue)
 // a way of processing data which uses simple primitives
-function nand_operation(a, b){
-	var c = 1-(a&b);
-	return c;
+function universal_operation(a, b){
+	return 1-(a&b);
 }
 
 var gate_queue = [];
 
 function xor_circuit(in_a, in_b){
 
-	memory=new Array(6); // 6 undefined elements
+	var memory=new Array(6); // 6 undefined elements
 	for(var i=0; i<memory.length; i++)
 		memory[i]=9;
 	memory[0]=in_a; memory[1]=in_b;
@@ -19,11 +18,11 @@ function xor_circuit(in_a, in_b){
 	p(memory);
 	
 	var gate_table=[
-		[0,2,3],	// 0 acd (nand gate)
-		[0,1,2],	// 1 abc (nand gate)
-		[3,4,5],	// 2 deq (nand gate)
-		[2,1,4],	// 3 cbe (nand gate)
-		[5,1],	// 4 qb (buffer gate)
+		[0,2,3],	// 0 acd (negated-and gate)
+		[0,1,2],	// 1 abc (negated-and gate)
+		[3,4,5],	// 2 deq (negated-and gate)
+		[2,1,4],	// 3 cbe (negated-and gate)
+		[5,1]	// 4 qb (buffer gate)
 	];
 	function get_gates_having_source(source){
 		var gates_by_source = [
@@ -32,60 +31,54 @@ function xor_circuit(in_a, in_b){
 			[0,3],	// 2
 			[2],		// 3
 			[2],		// 4
-			[/*4*/],		// 5
+			[/*4*/]		// 5
 		];
 		return gates_by_source[source];
 	}
 	//queue=[0];
 	
-	/*
-	process_queue([0]);
-	process_queue([1]);
-	for(var i=0; i<5; i++){
-		console.log(memory[5]);
-		memory[1]=memory[5];
-		process_queue([1]);
-	}
-	console.log(memory[5]);
-	*/
-	
-	/*
-	var gate_q = [
-		['q', [0] ], // queue A
-		['q', [1] ], // queue B
-		//
-		['o', [5] ], // out Q
-		['q', [1] ], // queue B
-		['o', [5] ], // out Q
-		['q', [1] ], // queue B
-		['o', [5] ], // out Q
-		//
-	];
-	*/
-	/*
-	var gate_list = [
-		['q', [2] ], // queue gate 2
-	];
-	
-	var commands = gate_list;
-	
-	commands.forEach(function(command){
-		var command_type = command[0];
-		var param1 = command[1];
-		if(command_type=='q'){
-			process_queue(param1);
-		}
-		else if(command_type=='o'){
-			param1.forEach(function(address){
-				p("out command: " + memory[address]);
-			});
-		}
-	});
-	*/
+	//	process_queue([0]);
+//	process_queue([1]);
+//	for(var i=0; i<5; i++){
+//		console.log(memory[5]);
+//		memory[1]=memory[5];
+//		process_queue([1]);
+//	}
+//	console.log(memory[5]);
 
-	process_queue();
+    //	var gate_q = [
+//		['q', [0] ], // queue A
+//		['q', [1] ], // queue B
+//		//
+//		['o', [5] ], // out Q
+//		['q', [1] ], // queue B
+//		['o', [5] ], // out Q
+//		['q', [1] ], // queue B
+//		['o', [5] ], // out Q
+//		//
+//	];
 
-	function process_queue(queue_param){
+    //	var gate_list = [
+//		['q', [2] ], // queue gate 2
+//	];
+//	];
+//	var commands = gate_list;
+//	var commands = gate_list;
+//	commands.forEach(function(command){
+//		var command_type = command[0];
+//		var param1 = command[1];
+//		if(command_type=='q'){
+//			process_queue(param1);
+//		}
+//		else if(command_type=='o'){
+//			param1.forEach(function(address){
+//				p("out command: " + memory[address]);
+//			});
+//		}
+//	});
+    process_queue();
+
+	function process_queue(){
 	//queue.concat(queue_param);
 	//input_queue=[0];
 	gate_queue=[1];
@@ -101,7 +94,7 @@ function xor_circuit(in_a, in_b){
 		}
 		*/
 		
-		gate=gate_queue.shift();
+		var gate=gate_queue.shift();
 		if(typeof gate=='undefined'){
 			gate_queue = [];
 			break; // exits on empty queue
@@ -111,22 +104,23 @@ function xor_circuit(in_a, in_b){
 		//gates=get_gates_having_source(gate);
 		//p('gates from source: '+gate+ ' gates: '+gates);
 		
-		process_gate = function(gate_index) { // for each: gate
+		var process_gate = function(gate_index) { // for each: gate
 			p("gate index: "+ gate_index);
 			var gate = gate_table[gate_index];
 			p("gate wiring: "+ gate);
 			//destination_before=memory[destination];
+            var destination, destination_before;
 			if(gate.length==2){
 				var source = gate[0];
-				var destination = gate[1];
+				destination = gate[1];
 				destination_before=memory[destination];
 				memory[destination] = memory[source];
 			}else if(gate.length==3){
 				var source1 = gate[0];
 				var source2 = gate[1];
-				var destination = gate[2];
+				destination = gate[2];
 				destination_before=memory[destination];
-				memory[destination]=nand_operation(memory[source1], memory[source2]);
+				memory[destination]=universal_operation(memory[source1], memory[source2]);
 			}
 			var destination_after=memory[destination];
 
@@ -153,7 +147,7 @@ function xor_circuit(in_a, in_b){
 	return out;
 
 }
-
+/*
 function xor_circuit_with_feedback(in_a, in_b){
 	var memory_array=[];
 	for(var i=0; i<10; i++) memory_array.push(0);
@@ -177,7 +171,7 @@ function xor_circuit_with_feedback(in_a, in_b){
 		var size = mapping[item].length;
 		var value;
 		if(size==3){
-			value=nand_operation(
+			value=universal_operation(
 				update(mapping[item][1]),
 				update(mapping[item][2])
 			);
@@ -211,7 +205,7 @@ function xor_circuit_with_feedback(in_a, in_b){
 	}
 	//return q;
 }
-
+*/
 /*
 p('xor_circuit implemented with queue system');
 p(xor_circuit(0, 0));
@@ -223,7 +217,7 @@ p(xor_circuit(1, 1));
 p('xor_circuit with feedback implemented with queue system' );
 xor_circuit(1, 0);
 
-function view(var_name, var_value=''){
+function view(var_name, var_value){
 	if(true || ['queue'].indexOf(var_name)!=-1){
 		p(var_name+': '+
 			(var_value===''?global[var_name]:var_value)
